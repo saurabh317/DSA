8459ee664d18cc4a15b5e80e431c006629dce707
saurabh317 <saurabhsp317@gmail.com>
2024-09-10 01:18:36 +0530

    added Array prgms

---
+++ src/Array/ArrayListExample.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {// it is used to implement a dynamic array with no given size
+    // syntax
+    // ArrayList<dataType> variable_name = new ArrayList<>()
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(4);
+        list.add(9);
+        System.out.println(list);
+
+    }
+
+}
