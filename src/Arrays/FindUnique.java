c9f5137e6ca626d169f75cd1ab264b68eb613b63
saurabh317 <saurabhsp317@gmail.com>
2024-09-14 00:14:44 +0530

    added find unique

--- src/Arrays/FindUnique.java
+++ src/Arrays/FindUnique.java
@@ -2,24 +2,23 @@ package Arrays;
 
 import java.util.ArrayList;
 
-public class FindUnique {
-    static void findUniqueNumbers(int[] arr) {
-//        int unique = arr[0];
-//        for (int i = 0; i < arr.length ; i++) {
-//
-//            for (int j = i + 1; j < arr.length; j++) {
-//                if(arr[i] == arr[j]){
-//                    unique = arr[j];
-//                }
-//            }
-//
-//        }
-//        System.out.println(unique);
+public class FindUnion {
+    static void findUnion (int[] arr1, int[] arr2) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j <arr2.length ; j++) {
+                if(arr1[i] == arr2[j]){
+                    temp.add(arr1[i]);
+                }
+            }
+        }
 
+        System.out.println(temp);
 
     }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 1, 2, 4, 5, 4};
-        findUniqueNumbers(arr);
+        int[] arr1 = {2, 5, 6, 9, 10, 18}; // 5, 6, 10
+        int[] arr2 = {5, 6, 10, 81, 91};
+        findUnion(arr1, arr2);
     }
 }
