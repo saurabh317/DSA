88c38317e920aba347b5158b3a63bc342b86df53
saurabh317 <saurabhsp317@gmail.com>
2024-09-15 01:43:23 +0530

    majority element in an array

--- src/Arrays/MajorityElement.java
+++ src/Arrays/MajorityElement.java
@@ -1,4 +1,25 @@
 package Arrays;
 
 public class MajorityElement {
+    static void findMajority (int[] arr) {
+        int half = arr.length/2;
+        int counter = 0;
+        for (int k : arr) {
+            for (int i : arr) {
+                if (k == i) {
+                    counter++;
+                }
+            }
+            if (counter >= half) {
+                System.out.println(k);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,4,2,4,3,4,4,3,2,4};
+        findMajority(arr);
+
+    }
 }
