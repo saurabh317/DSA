afe3bd60038f76d7fb96aa3d0c5a5727d075eddc
saurabh317 <saurabhsp317@gmail.com>
2024-10-03 00:23:30 +0530

    added BubbleSort

--- src/Arrays/Sorting/BubbleSort.java
+++ src/Arrays/Sorting/BubbleSort.java
@@ -1,4 +1,30 @@
 package Arrays.Sorting;
 
-public class BubbleSort {
+import java.util.Arrays;
+
+public class BubbleSort { // bubble sort is often know as sinking or exchange sort
+    public static int[] sortArr (int[] arr) {
+        int loopCounter = arr.length - 1;
+        boolean swaped = false;
+        while (loopCounter > 0) {
+            for (int i = 0; i < arr.length - 1 ; i++) {
+                int temp;
+                if(arr[i] > arr[i+1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    swaped = true;
+                }
+            }
+            if (!swaped) break; // best case if in a particular iteration swap doesn't happen then it means that arr is sorted
+            loopCounter--;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2,3, 4};// {1, 2,3, 4} {3,1,5,4,2}
+        System.out.println((Arrays.toString(sortArr(arr))));
+    }
 }
