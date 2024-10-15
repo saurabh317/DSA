92ec47ef4f9c5141da54bcd2ff0f6951a9a74a7d
saurabh317 <saurabhsp317@gmail.com>
2024-10-15 23:46:42 +0530

    string and its properties

---
+++ src/String/Basics.java
@@ -0,0 +1,34 @@
+package String;
+
+import java.util.Scanner;
+
+public class Basics {
+    public static void takeInputAndPrint () {
+        System.out.println("enter your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(" thank you " + name + " for your time");
+    }
+
+    public static void preetyPrinting () {
+        // print the value of pi but up to 3 values after .
+        System.out.printf("formatted value of PI is %.3f %n", Math.PI);
+
+        // print the round of the given number
+        float num = 1.345678F;
+        System.out.printf("round of the given number is %.2f", num);
+    }
+
+    public static void main(String[] args) {
+        String name1 = "harry";
+        String name2 = "harry";
+        String name3 = new String("john");
+        String name4 = new String("john");
+        System.out.println(name1 == name2); // this is returning true coz the values are stored in string pool
+        System.out.println(name3 == name4); // this is returning false coz every time we are creating a new obj
+
+
+        takeInputAndPrint();
+        preetyPrinting();
+    }
+}
